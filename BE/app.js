6405584d49bb9d59e785bae711dc7b9ede8c023b
Quoc_Anh <dohuuquocanh21dk@gmail.com>
2026-09-17 15:13:31 +0700

    Update

--- BE/app.js
+++ BE/app.js
@@ -35,7 +35,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true, limit: '2mb' }));
 
